9bf461221f25578eb1ad4135268ced889a24c985
Marilou de Courville <marilou@courville.org>
2023-10-02 16:27:11 +0200

    update random

---
+++ snake-the-game/genetic_algorithm.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Genetic_Algorithm:
+    
+    def calculate_fitness(self, population: list) -> list:
+        pass
+
+    def select_mates(self, population: list, fitness: list) -> list:
+        pass
+
+    def crossover(self, population: list, fitness: list) -> list:
+        pass
+
+    def mutate(self, population: list, fitness: list) -> list:
+        pass
+
+
+'''
+il faut trouver
+* une façon de timer les snakes dans game collection
+* une façon de récupérer les scores des snakes dans game collection
+* une façon de récumérer les caractéristiques des snakes pour la mutation
+
+'''
