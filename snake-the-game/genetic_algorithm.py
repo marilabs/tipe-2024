ddb2919eee7b852c544051260d51084662bde960
Marilou de Courville <marilou@courville.org>
2023-11-20 15:40:48 +0100

    updates w antoine's suggestions

--- snake-the-game/genetic_algorithm.py
+++ snake-the-game/genetic_algorithm.py
@@ -5,6 +5,9 @@ from neural_network import NeuralNetwork
 class GeneticAlgorithm:
 
     save_bests = 0
+    k = 5
+    mut_chance = 0.1
+    coeff = 0.1
 
     def __init__(self, save_bests: int) -> None:
         self.save_bests = save_bests
@@ -13,18 +16,17 @@ class GeneticAlgorithm:
         # Roulette-wheel selection: numpy.random.choice
         maxi = sum([x[1] for x in population])
         selection_probability = [x[1] / maxi for x in population]
-        parent = np.random.choice(len(population), p=selection_probability)
-        return population[parent][0]
+        parent1, parent2 = np.random.choice(len(population), p=selection_probability), np.random.choice(len(population), p=selection_probability)
+        return population[parent1][0], population[parent2][0]
 
-    def crossover(parent_a: [float], parent_b: [float]) -> [float]:
+    def crossover(self, parent_a: [float], parent_b: [float]) -> [float]:
         """
         K-point crossover cf Wikipedia:
         - select k random points in range(len(parent_a))
         - create a new array which alternate between coefficients of parent_a and parent_b
         """
         n = len(parent_a)
-        k = 5
-        l = sorted([np.random.randint(0, n) for i in range(k)])
+        l = sorted([np.random.randint(0, n) for _ in range(self.k)])
         child = []
         current_parent = 0
         current_index = 0
@@ -38,7 +40,7 @@ class GeneticAlgorithm:
                 child.append(parent_b[i])
         return child
 
-    def mutate(genome: [float]) -> None:
+    def mutate(self, genome: [float]) -> None:
         """
         Gaussian mutation:
         - for each coefficient:
@@ -47,13 +49,11 @@ class GeneticAlgorithm:
                 - generate an amplitude (between 0 and 1)
                 - add sign * amplitude * coeff to the coefficient (coeff is a parameter)
         """
-        mut_chance = 0.1
-        coeff = 0.1
         for g in genome:
-            if np.random() <= mut_chance:
-                sign = 1 if np.random() <= 0.5 else -1
-                amplitude = np.random()
-                g += sign * amplitude * coeff
+            if np.random.random() <= self.mut_chance:
+                sign = 1 if np.random.random() <= 0.5 else -1
+                amplitude = np.random.random()
+                g += sign * amplitude * self.coeff
 
     def evolve(self, population: [NeuralNetwork, int]) -> list:
         assert(len(population) != 0)
