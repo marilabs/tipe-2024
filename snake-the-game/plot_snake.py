73ac374f2befb10fcf70b7e30f6ef5aa82dae1db
Marilou de Courville <marilou@courville.org>
2024-06-02 12:51:28 +0200

    plot: better convergence curves

--- snake-the-game/plot_snake.py
+++ snake-the-game/plot_snake.py
@@ -1,14 +1,6 @@
-import pygame
-import os
-import signal
-import sys
 import pickle
-from game import Game
-import math
 import matplotlib.pyplot as plt
-import numpy as np
 import config as c
-from neural_network import NeuralNetwork
 
 max_fitness = []
 min_fitness = []
@@ -23,7 +15,43 @@ def restore_curves(filename):
         data = pickle.load(f)
     return data
 
-(max_fitness, min_fitness, avg_fitness, max_apple_eaten, min_apple_eaten, avg_apple_eaten, max_snake_length) = restore_curves(c.CURVES_FILES)
+(max_fitness, min_fitness, avg_fitness, max_apple_eaten, min_apple_eaten, avg_apple_eaten, max_snake_length) = restore_curves("curve.pickle")
+
+fig, ax1 = plt.subplots()
+
+color1 = 'tab:blue'
+ax1.set_xlabel('Itération')
+ax1.set_ylabel('Fitness maximum', color=color1)
+ax1.set_yscale('log')
+ax1.plot(range(len(max_fitness)), max_fitness, color=color1)
+ax1.tick_params(axis='y', labelcolor=color1)
+
+color3 = 'tab:green'
+ax2 = ax1.twinx()
+ax2.set_ylabel('Pommes mangées maximum', color=color3)
+ax2.plot(range(len(max_apple_eaten)), max_apple_eaten, color=color3)
+ax2.tick_params(axis='y', labelcolor=color3)
+
+plt.title('Fitness vs Iteration')
+# Add Vertical Gridlines (The Key Change)
+ax1.grid(axis='x', linestyle='--')  # Gridlines on the x-axis (iterations)
+ax2.grid(axis='y', linestyle='--')  # You need to add it for the second axis too
+fig.tight_layout()
+plt.savefig("curve.svg")
+plt.savefig("curve.eps")
+plt.savefig("curve.pdf")
+plt.show()
+
+
+
+"""
+color2 = 'tab:red'
+ax2 = ax1.twinx()
+ax2.set_ylabel('Fitness moyenne', color=color2)
+ax2.plot(range(len(avg_fitness)), avg_fitness, color=color2)
+ax2.tick_params(axis='y', labelcolor=color2)
+"""
+
 
 """
 # Set the y-axis limits from 0 to max_fitness_value
@@ -40,6 +68,7 @@ plt.legend()
 plt.show()
 """
 
+"""
 fig, ax1 = plt.subplots()
 
 color = 'tab:blue'
@@ -64,26 +93,4 @@ fig.tight_layout()
 plt.title('Fitness moyenne/maximum fct itération')
 plt.grid(True)
 plt.show()
-
-
-fig, ax1 = plt.subplots()
-
-color1 = 'tab:blue'
-ax1.set_xlabel('Itération')
-ax1.set_ylabel('Fitness maximum', color=color1)
-ax1.plot(range(len(max_fitness)), max_fitness, color=color1)
-ax1.tick_params(axis='y', labelcolor=color1)
-
-color2 = 'tab:red'
-ax2 = ax1.twinx()
-ax2.set_ylabel('Fitness moyenne', color=color2)
-ax2.plot(range(len(avg_fitness)), avg_fitness, color=color2)
-ax2.tick_params(axis='y', labelcolor=color2)
-
-color3 = 'tab:green'
-ax1.plot(range(len(max_apple_eaten)), max_apple_eaten, color=color3, linestyle='dashed', label='Pommes manées maximum')
-
-fig.tight_layout()
-plt.title('Fitness vs Iteration')
-plt.grid(True)
-plt.show()
+"""
